6789189c99dd1fec410e0cd443ae590b775968fa
Gabriel Teixeira de Carvalho <70416214+GabrielT31xeira@users.noreply.github.com>
2023-04-27 16:40:59 -0300

    Primeiro commit

---
+++ Data/atividade10Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using atividade10.Models;
+
+namespace atividade10.Data
+{
+    public class atividade10Context : DbContext
+    {
+        public atividade10Context (DbContextOptions<atividade10Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<atividade10.Models.Movie> Movie { get; set; } = default!;
+    }
+}
